67b94324689c5a7647f79e5c19b57eb6b48c819e
Maks <panasenkomaksim@gmail.com>
2020-09-19 10:16:32 +0300

    Start refactoring code in class ConnectVPN

--- src/vpn/ConnectVPN.java
+++ src/vpn/ConnectVPN.java
@@ -4,22 +4,25 @@ import java.awt.Component;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 
 public class ConnectVPN {
 
     private DataConnect dataConnect;
     private Component comp;
-
-    private InputStreamReader input;
-    private OutputStreamWriter output;
     private Process process;
+    private boolean statusConnect = false;
 
     public ConnectVPN(Component comp, DataConnect dc) {
         this.comp = comp;
         this.dataConnect = dc;
+    }
+
+    public boolean getStatusConnect() {
+        return statusConnect;
     }
 
     public DataConnect getDataConnect() {
@@ -40,7 +43,7 @@ public class ConnectVPN {
         try {
             process = new ProcessBuilder(cmd).start();
 
-            runWithPrivileges(process);
+            inputStream(process);
 
         } catch (IOException ex) {
             Logger.getLogger(ConnectVPN.class.getName()).log(Level.SEVERE, null, ex);
@@ -51,32 +54,18 @@ public class ConnectVPN {
 
     }
 
-    public boolean runWithPrivileges(Process process) {
+    private boolean inputStream(Process process) {
 
         try {
-
-            output = new OutputStreamWriter(process.getOutputStream());
-            input = new InputStreamReader(process.getInputStream());
-            
+            InputStreamReader input = new InputStreamReader(process.getInputStream());
 
             int bytes, tryies = 0;
             char buffer[] = new char[1024];
             while ((bytes = input.read(buffer, 0, 1024)) != -1) {
-                System.out.println("23");
                 if (bytes == 0) {
                     continue;
                 }
-                String data = String.valueOf(buffer, 0, bytes);
-                System.out.println(data);
-
-                if (data.contains("[sudo] пароль")) {
-                    char password[] = new char[]{'5', '4', '8', '9', '8', '7'};
-                    output.write(password);
-                    output.write('\n');
-                    output.flush();
-                    Arrays.fill(password, '\0');
-                    tryies++;
-                }
+                outputStream(String.valueOf(buffer, 0, bytes));
             }
 
             return tryies < 3;
@@ -85,6 +74,68 @@ public class ConnectVPN {
         }
 
         return false;
+    }
+//    private boolean inputStream(Process process) {
+//
+//        try {
+//            InputStreamReader input = new InputStreamReader(process.getInputStream());
+//
+//            int bytes, tryies = 0;
+//            char buffer[] = new char[1024];
+//            while ((bytes = input.read(buffer, 0, 1024)) != -1) {
+//                if (bytes == 0) {
+//                    continue;
+//                }
+//                outputStream(String.valueOf(buffer, 0, bytes));
+//            }
+//
+//            return tryies < 3;
+//        } catch (IOException ex) {
+//            System.out.println(ex);
+//        }
+//
+//        return false;
+//    }
+//
+//    private void outputStream(String data) {
+//        OutputStreamWriter output = new OutputStreamWriter(process.getOutputStream());
+//
+//        if (data.contains("[sudo] пароль")) {
+//            try {
+//                char password[] = new char[]{'5', '4', '8', '9', '8', '7'};
+//                output.write(password);
+//                output.write('\n');
+//                output.flush();
+//                Arrays.fill(password, '\0');
+//            } catch (IOException ex) {
+//                Logger.getLogger(ConnectVPN.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        }
+//
+//    }
+    
+    private void outputStream(String data) {
+        OutputStreamWriter output = new OutputStreamWriter(process.getOutputStream());
+
+        if (data.contains("[sudo] пароль")) {
+            try {
+                char password[] = passwordDialog();
+                output.write(password);
+                output.write('\n');
+                output.flush();
+            } catch (IOException ex) {
+                Logger.getLogger(ConnectVPN.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+
+    public char[] passwordDialog() {
+        JPasswordField pwd = new JPasswordField(10);
+
+        int option = JOptionPane.showConfirmDialog(null, new JPasswordField(10), "Confirm root password", JOptionPane.OK_CANCEL_OPTION);
+        return pwd.getPassword();
+
     }
 
 }

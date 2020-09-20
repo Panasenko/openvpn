d2ba3b54cf9f6f9191579ace033da0e2c499d6f9
Maks <panasenkomaksim@gmail.com>
2020-09-20 23:20:46 +0300

    Добавлена сериализация и десиреализация

---
+++ src/vpn/FileUtils.java
@@ -0,0 +1,106 @@
+package vpn;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+
+public class FileUtils {
+
+    private static String FILE_NAME = "/AUTH.txt";
+
+    public static void addFileFilter(JFileChooser jfch, FileFilter ff) {
+        jfch.removeChoosableFileFilter(jfch.getFileFilter());
+        jfch.setFileFilter(ff);
+        jfch.setSelectedFile(new File(""));
+    }
+
+    public static File genereteTempFile() {
+        File file = null;
+
+        try {
+            String path = new File("").getCanonicalPath();
+
+            System.out.println("Путь " + path);
+            file = new File(path + FILE_NAME);
+            file.setWritable(true);
+            file.setReadable(true);
+        } catch (IOException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return file;
+    }
+
+    public static boolean fileWriter(File file, String data) {
+
+        System.out.println(data);
+        FileWriter writer;
+        try {
+            writer = new FileWriter(file);
+
+            writer.append(data);
+            writer.flush();
+            writer.close();
+            return true;
+
+        } catch (IOException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return false;
+
+    }
+
+    public static boolean deleteTempFile(File file) {
+        if (file != null) {
+            return file.delete();
+        }
+
+        return false;
+    }
+
+    public static void serializable(Object obj, String fileParh) {
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(fileParh);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(obj);
+            objectOutputStream.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                fileOutputStream.close();
+            } catch (IOException ex) {
+                Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+    
+    public static Object deserialize(String fileName) {
+        try {
+            FileInputStream fis = new FileInputStream(fileName);
+            ObjectInputStream oin = new ObjectInputStream(fis);
+            Object ts = (Object) oin.readObject();
+            fis.close();
+            return ts;
+
+        } catch (ClassNotFoundException | IOException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+
+}
